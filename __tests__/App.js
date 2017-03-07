66886580cf14f9486a4ee6b3d5c0c46c9aa47422
Albert Brand <abrand@xebia.com>
2017-03-07 17:47:58 +0100

    React class mock implementation

--- __tests__/App.js
+++ __tests__/App.js
@@ -4,7 +4,15 @@ import renderer from 'react-test-renderer';
 
 import App from '../App';
 
-jest.mock('react-native-vector-icons/FontAwesome', () => (props) => mockReact.createElement('Icon', props, props.children));
+jest.mock('react-native-vector-icons/FontAwesome', () => {
+  class Icon extends mockReact.Component {
+    render() {
+      const { children, ...props } = this.props;
+      return mockReact.createElement('Icon', props, children);
+    }
+  }
+  return Icon;
+});
 
 describe('App', () => {
   it('renders correctly', () => {
